6d5a7419b243333ca6a75891f6ecb5b6e3773977
David Antunes <davaantunes@gmail.com>
2024-01-05 18:35:30 +0000

    fixed list_events

--- client/api.c
+++ client/api.c
@@ -101,22 +101,22 @@ int ems_quit() {
 }
 
 int ems_create(unsigned int event_id, size_t num_rows, size_t num_cols) {
-  int exit;
+  int ret;
   char code = OP_CREATE;
   send_msg(request_pipe, &code, sizeof(char));
   send_msg(request_pipe, &session_id, sizeof(int));
   send_msg(request_pipe, &event_id, sizeof(unsigned int));
   send_msg(request_pipe, &num_rows, sizeof(size_t));
   send_msg(request_pipe, &num_cols, sizeof(size_t));
-  get_msg(response_pipe, &exit, sizeof(int));
+  get_msg(response_pipe, &ret, sizeof(int));
   // TODO: send create request to the server (through the request pipe) and wait
   // for the response (through the response pipe)
-  return exit;
+  return ret;
 }
 
 int ems_reserve(unsigned int event_id, size_t num_seats, size_t *xs,
                 size_t *ys) {
-  int exit;
+  int ret;
   char code = OP_RESERVE;
   send_msg(request_pipe, &code, sizeof(char));
   send_msg(request_pipe, &session_id, sizeof(int));
@@ -124,20 +124,20 @@ int ems_reserve(unsigned int event_id, size_t num_seats, size_t *xs,
   send_msg(request_pipe, &num_seats, sizeof(size_t));
   send_msg(request_pipe, xs, sizeof(size_t) * num_seats);
   send_msg(request_pipe, ys, sizeof(size_t) * num_seats);
-  get_msg(response_pipe, &exit, sizeof(int));
+  get_msg(response_pipe, &ret, sizeof(int));
   // TODO: send reserve request to the server (through the request pipe) and
   // wait for the response (through the response pipe)
-  return exit;
+  return ret;
 }
 
 int ems_show(int out_fd, unsigned int event_id) {
-  int exit;
+  int ret;
   char code = OP_SHOW;
   send_msg(request_pipe, &code, sizeof(char));
   send_msg(request_pipe, &session_id, sizeof(int));
   send_msg(request_pipe, &event_id, sizeof(unsigned int));
-  get_msg(response_pipe, &exit, sizeof(int));
-  if (exit == 0) {
+  get_msg(response_pipe, &ret, sizeof(int));
+  if (ret == 0) {
     size_t num_rows, num_cols;
     get_msg(response_pipe, &num_rows, sizeof(size_t));
     get_msg(response_pipe, &num_cols, sizeof(size_t));
@@ -158,16 +158,16 @@ int ems_show(int out_fd, unsigned int event_id) {
     }
     free(seats);
   }
-  return exit;
+  return ret;
 }
 
 int ems_list_events(int out_fd) {
-  int exit;
+  int ret;
   char code = OP_LIST_EVENTS;
   send_msg(request_pipe, &code, sizeof(char));
   send_msg(request_pipe, &session_id, sizeof(int));
-  get_msg(response_pipe, &exit, sizeof(int));
-  if (exit == 0) {
+  get_msg(response_pipe, &ret, sizeof(int));
+  if (ret == 0) {
     size_t num_events;
     get_msg(response_pipe, &num_events, sizeof(size_t));
     unsigned int *ids = malloc(num_events * sizeof(unsigned int));
@@ -179,12 +179,13 @@ int ems_list_events(int out_fd) {
     }
     free(ids);
   }
-  else if (exit == 2) {
+  else if (ret == 2) {
     char no_events[11] = "No Events\n";
     write(out_fd, no_events, strlen(no_events));
+    ret = 0;
   }
   // TODO: send list request to the server (through the request pipe) and wait
   // for the response (through the response pipe)
   // TODO: write output to file
-  return exit;
+  return ret;
 }
